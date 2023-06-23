f942e4a9d6b00eea653635ac1e5ad25f58ee1eb2
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-06-23 22:51:54 +0700

    Script.lua

--- Script.lua
+++ Script.lua
@@ -512,10 +512,7 @@ end
             setHexMemory("libil2cpp.so", 0x1AE7F08, "01 00 A0 E3")
 
             setHexMemory("libil2cpp.so", 0x1AE7F0C, "1E FF 2F E1")
- setHexMemory("libil2cpp.so", 0x1AE9CA4, "01 00 A0 E3")
-
-            setHexMemory("libil2cpp.so", 0x1AE9CA8, "1E FF 2F E1")
-            
+ 
             
             
 gg.toast("Show Unti On✅")
