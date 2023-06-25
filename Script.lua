1074a7230f66f8c4a5beb567679997842e65f74c
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-06-25 08:07:26 +0700

    Script.lua

--- Script.lua
+++ Script.lua
@@ -47,7 +47,7 @@ menu = gg.multiChoice({
 "👉Show Unti",
 
 "👉Cam xa x1",
-"👉Cam xa x2",
+
 "👉Bypass Report [Trận]",
 
 "Thoát"
@@ -74,7 +74,6 @@ if menu[8] == true then a8() end
 
 if menu[9] == true then a9() end
 
-if menu[10] == true then a10() end
 end
 
 XGCK = -1
@@ -562,53 +561,10 @@ gg.toast("Cam xa 1x On✅")
 
 end
 
+
+
+
 function a8()
-local n, startAddress, endAddress = nil, 0, 0
-local function name(lib)
- if n == lib then
-  return startAddress, endAddress
- end
- local ranges = gg.getRangesList(lib or 'libil2cpp.so')
- for i, v in ipairs(ranges) do
-  if v.state == "Xa" then
-   startAddress = v.start
-   endAddress = ranges[#ranges]['end']
-   break
-  end
- end
- return startAddress, endAddress
-end
-local function setHexMemory(libname, offset, hex)
- name(libname)
- local t, total = {}, 0
- for h in string.gmatch(hex, "%S%S") do
-     table.insert(t, {
-         address = startAddress + offset + total,
-         flags = gg.TYPE_BYTE,
-         value = h .. "r"
-     })
-     total = total + 1
- end
- local res = gg.setValues(t)
- if type(res) ~= 'string' then
-  return trueo
- else
-  gg.alert(res)
-  return false
- end
-end
-
-            setHexMemory("libil2cpp.so", 0x18A2A04, "75 01 A0 E3 1E FF 2F E1")
-           setHexMemory("libil2cpp.so", 0x18A2A08, "75 01 A0 E3 1E FF 2F E1")
-       
-            
-            
-gg.toast("Cam xa 2x On✅")
-
-end
-
-
-function a9()
 
 gg.clearResults()
 
@@ -658,7 +614,7 @@ end
 
 
 
-function a10()
+function a9()
 os.exit()
 
 end
