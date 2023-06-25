b256ab84ea48662274d0cd217b7e717c57b46b53
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-06-25 22:18:33 +0700

    Script.lua

--- Script.lua
+++ Script.lua
@@ -503,12 +503,7 @@ local function setHexMemory(libname, offset, hex)
 end
 
 
- setHexMemory("libil2cpp.so", 0x170458C, "00 00 A0 E3 1E FF 2F E1")
-setHexMemory("libil2cpp.so", 0x171039C, "00 00 A0 E3 1E FF 2F E1")
-setHexMemory("libil2cpp.so", 0x18EF804, "00 00 A0 E3 1E FF 2F E1")
-setHexMemory("libil2cpp.so", 0x197C6D0, "00 00 A0 E3 1E FF 2F E1")
-setHexMemory("libil2cpp.so", 0x1AE7DB4, "00 00 A0 E3 1E FF 2F E1")
-setHexMemory("libil2cpp.so", 0x1AE7F08, "00 00 A0 E3 1E FF 2F E1")
+ setHexMemory("libil2cpp.so", 0x1AE7F08, "01 00 A0 E3 1E FF 2F E1")
  
             
 gg.toast("Show Unti On✅")
