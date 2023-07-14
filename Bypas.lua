7c1e97323c2f31ca3fc3e967dd29779de841c426
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-07-15 00:48:54 +0700

    Bypas.lua

--- Bypas.lua
+++ Bypas.lua
@@ -17,4 +17,23 @@ Modify(Read('libanogs.so:bss','rw-p')+0x638,4096,4)
 Modify(Read('libanogs.so:bss','rw-p')+0x6c8,4096,4)
 Modify(Read('libanogs.so:bss','rw-p')+0x1948,4096,4)
 Modify(Read('libanogs.so:bss','rw-p')+0x6210,2,4)
+
+function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
+so=gg.getRangesList('libanogs.so')[1].start
+
+
+setvalue(so + "0xCB46C", 4, "h 00 00 00 00")
+setvalue(so + "0xCB470", 4, "h 00 00 00 00")
+setvalue(so + "0xCB474", 4, "h 00 00 00 00")
+setvalue(so + "0xCB478", 4, "h 00 00 00 00")
+
+setvalue(so + "0xCB504", 4, "h 00 00 00 00")
+setvalue(so + "0xCB508", 4, "h 00 00 00 00")
+setvalue(so + "0xCB4A4", 4, "h 00 00 00 00")
+setvalue(so + "0xCB390", 4, "h 00 00 00 00")
+setvalue(so + "0xCB394", 4, "h 00 00 00 00")
+setvalue(so + "0xCB3DC", 4, "h 00 00 00 00")
+setvalue(so + "0x108969", "hsub_108968", "osub_108968")
+
+
 gg.setValues(Table)
