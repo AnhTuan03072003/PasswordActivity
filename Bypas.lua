042caba7b5c2888d0905d275e3185ea894281018
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-07-14 23:16:06 +0700

    Bypas.lua

--- Bypas.lua
+++ Bypas.lua
@@ -1,23 +1,33 @@
+function HOME()
+onan1st = gg.multiChoice({
+" CRASH FIX 32BIT ",
+" BYPASS ", 
+"  EXIT "
+}, nil, (os.date("SANSERO CRASH FIX VPHONE \n CRASH FIX 64 / 32 \n┗⊳🔲 • %A, %d %B %Y %H:%M%p  ")))
+if onan1st == nil then
+  else
+if onan1st[1] == true then BP1()end
+if onan1st[1] == true then BP2()end
+if onan1st[2] == true then EXIT()end
+end
+PUBGMH = -1
+end
+
+function BP1()
+gg.processPause()
 function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end 
 so=gg.getRangesList('libanogs.so')[1].start
-setvalue(so + "0x14EED8 ", 4, "h C0 03 5F D6")
-setvalue(so + "0x349F1C ", 4, "h C0 03 5F D6")
-gg.toast('Fix Crash Sansero + Protect Done ')
+setvalue(so + "0xACA12", 4, "h 00 20 70 47")--Crash Fix
+setvalue(so + "0x27B236", 4, "h 00 20 70 47")--Crash Fix
+gg.alert('Crash Fix Done')
+gg.processResume()
+end
 
-function setvalue(address,flags,value)  local refinevalues={}  refinevalues[1]={}  refinevalues[1].address=address  refinevalues[1].flags=flags  refinevalues[1].value=value  gg.setValues(refinevalues)  end 
-
-
-so=gg.getRangesList('libanogs.so')[1].start 
-py=0x5093A8 setvalue(so+py,4,4096)
-py=0x509638 setvalue(so+py,4,4096)
-py=0x5096C8 setvalue(so+py,4,4096)
-py=0x50A948 setvalue(so+py,4,4096)
-py=0x50F210 setvalue(so+py,4,4096)
-
-
-gg.isVisible(false)
+function BP2()
 function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
 so=gg.getRangesList('libanogs.so')[1].start
+
+
 
 setvalue(so + "0xCB46C", 4, "h 00 00 00 00")
 setvalue(so + "0xCB470", 4, "h 00 00 00 00")
@@ -44,7 +54,31 @@ setvalue(so + "0x3D6A96", 4, "h 00 00 00 00")
 setvalue(so + "0x3D6A7F", 4, "h 00 00 00 00")
 
 
+
+
+
+gg.getResults(9999)
+gg.editAll(101384192, gg.TYPE_DWORD, true, SIGN_EQUAL, 0,-1)
+gg.clearResults(9999)
 gg.setVisible(true)
+end
 
+function EXIT()
+print("  ┏━━┓♡☆")
+print("┏┛□□┗┓♡")
+print("┗⊙━━⊙┛==")
+gg.skipRestoreState()
+gg.setVisible(true)
+os.exit()
+end
 
-
+cs = "21"
+while true do
+if gg.isVisible(true) then
+PUBGMH = 1
+gg.setVisible(false)
+end
+if PUBGMH == 1 then
+HOME()
+end
+end
