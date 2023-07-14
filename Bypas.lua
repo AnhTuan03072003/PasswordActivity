9aad01f05432b63837720e82549fdf4b12cc6b27
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-07-15 00:07:57 +0700

    Bypas.lua

--- Bypas.lua
+++ Bypas.lua
@@ -1,14 +1,3 @@
-function setvalue(address,flags,value)  local refinevalues={}  refinevalues[1]={}  refinevalues[1].address=address  refinevalues[1].flags=flags  refinevalues[1].value=value  gg.setValues(refinevalues)  end 
-
-
-so=gg.getRangesList('libanogs.so')[1].start 
-py=0x5093A8 setvalue(so+py,4,4096)
-py=0x509638 setvalue(so+py,4,4096)
-py=0x5096C8 setvalue(so+py,4,4096)
-py=0x50A948 setvalue(so+py,4,4096)
-py=0x50F210 setvalue(so+py,4,4096)
-
-
 
 function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end 
 so=gg.getRangesList('libanogs.so')[1].start
