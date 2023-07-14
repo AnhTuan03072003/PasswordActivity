3037fb681d598c7a6df467ef9d41260c92d7e1ba
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-07-14 19:46:15 +0700

    bypass.lua

--- bypass.lua
+++ bypass.lua
@@ -1,1 +1,20 @@
-【P】a【P】
+local Ranges=gg.getRangesList('/')
+local function Read(module,type)
+    for k,v in pairs(Ranges) do
+        if v['internalName']:match('[^/]*$')==module and v['type']==type then
+            return v['start']
+        end
+    end
+end
+
+local Table={}
+local function Modify(address,value,flags)
+    Table[#Table+1]={address=address,value=value,flags=flags}
+end
+
+Modify(Read('libanogs.so:bss','rw-p')+0x3a8,4096,4)
+Modify(Read('libanogs.so:bss','rw-p')+0x638,4096,4)
+Modify(Read('libanogs.so:bss','rw-p')+0x6c8,4096,4)
+Modify(Read('libanogs.so:bss','rw-p')+0x1948,4096,4)
+Modify(Read('libanogs.so:bss','rw-p')+0x6210,2,4)
+gg.setValues(Table)
