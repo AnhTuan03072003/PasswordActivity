e9d665844cbe983202af02acb0bdb179e0d4808f
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-07-15 00:59:07 +0700

    Bypas.lua

--- Bypas.lua
+++ Bypas.lua
@@ -1,23 +1,4 @@
-local Ranges=gg.getRangesList('/')
-local function Read(module,type)
-    for k,v in pairs(Ranges) do
-        if v['internalName']:match('[^/]*$')==module and v['type']==type then
-            return v['start']
-        end
-    end
-end
 
-local Table={}
-local function Modify(address,value,flags)
-    Table[#Table+1]={address=address,value=value,flags=flags}
-end
-
-Modify(Read('libanogs.so:bss','rw-p')+0x3a8,4096,4)
-Modify(Read('libanogs.so:bss','rw-p')+0x638,4096,4)
-Modify(Read('libanogs.so:bss','rw-p')+0x6c8,4096,4)
-Modify(Read('libanogs.so:bss','rw-p')+0x1948,4096,4)
-Modify(Read('libanogs.so:bss','rw-p')+0x6210,2,4)
-gg.toast('Crash Fix Done')
 function HOME()
 onan1st = gg.multiChoice({
 " CRASH FIX ",
@@ -35,7 +16,13 @@ end
 
 function BP1()
 gg.processPause()
-
+function setvalue(address,flags,value)  local refinevalues={}  refinevalues[1]={}  refinevalues[1].address=address  refinevalues[1].flags=flags  refinevalues[1].value=value  gg.setValues(refinevalues)  end 
+so=gg.getRangesList('libanogs.so')[1].start 
+py=0x5093A8 setvalue(so+py,4,4096)
+py=0x509638 setvalue(so+py,4,4096)
+py=0x5096C8 setvalue(so+py,4,4096)
+py=0x50A948 setvalue(so+py,4,4096)
+py=0x50F210 setvalue(so+py,4,4096)
 gg.processResume()
 end
 
