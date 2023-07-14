031c4698e1922faab371467abce47f439c557a79
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-07-14 22:43:36 +0700

    Bypas.lua

--- Bypas.lua
+++ Bypas.lua
@@ -1,3 +1,9 @@
+function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end 
+so=gg.getRangesList('libanogs.so')[1].start
+setvalue(so + "0x14EED8 ", 4, "h C0 03 5F D6")
+setvalue(so + "0x349F1C ", 4, "h C0 03 5F D6")
+gg.toast('Fix Crash Sansero + Protect Done ')
+
 function setvalue(address,flags,value)  local refinevalues={}  refinevalues[1]={}  refinevalues[1].address=address  refinevalues[1].flags=flags  refinevalues[1].value=value  gg.setValues(refinevalues)  end 
 
 
