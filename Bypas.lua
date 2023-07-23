82e1d852ba401ba020981c0c04dc3445abc444df
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-07-23 14:45:09 +0700

    Bypas.lua

--- Bypas.lua
+++ Bypas.lua
@@ -1,247 +1,41 @@
-local function pass(...)
-
-arg = {...}
-
-local pas = gg.prompt
-
-({"🔑 KEY ĐƯỢC CẬP NHẬT LÚC: 16/07/2023 | 12H TRƯA MỖI NGÀY RESET KEY\n⚠️ VUI LÒNG XEM VIDEO HƯỚNG DẪN TRƯỚC KHI DÙNG\n👉 20K/1 THÁNG FREE KEY IB ADMIN TELEGRAM"}, {},{"text"})
-
-pas = pas and pas[1] .. " " or os.exit(gg.alert("Thoát... Ấn Oke",""))
-
-for i = 1, #arg do
-
-if arg[i] .. " " ==  pas then
-
-return gg.alert("Nhập Key Thành Công ✅", "")
-
-end
-
-end
-
-while (true) do
-
-os.exit(gg.alert("Key Sai Hoặc Hết Hạn❎", ""))
-
-end 
-
-end
-
-pass("hmghackk_hdbehe6262","t")
-
-
-
-
-
 function HOME()
 onan1st = gg.multiChoice({
-"CHỐNG CRASH + CHECK TP+84 [LOGO 2]",
-"BYPASS ISLAND",
-"  EXIT "
+" CRASH + CHỐNG CHECK [LOGO 2]",
 }, nil, (os.date("👍")))
 if onan1st == nil then
   else
 if onan1st[1] == true then BP1()end
-if onan1st[2] == true then BP2()end
-if onan1st[3] == true then EXIT()end
 end
 PUBGMH = -1
 end
 
 function BP1()
-gg.processPause()
 
-function S_Pointer(t_So, t_Offset, _bit)
-	local function getRanges()
-		local ranges = {}
-		local t = gg.getRangesList('^/data/*.so*$')
-		for i, v in pairs(t) do
-			if v.type:sub(2, 2) == 'w' then
-				table.insert(ranges, v)
-			end
-		end
-		return ranges
-	end
-	local function Get_Address(N_So, Offset, ti_bit)
-		local ti = gg.getTargetInfo()
-		local S_list = getRanges()
-		local _Q = tonumber(0x167ba0fe)
-		local t = {}
-		local _t
-		local _S = nil
-		if ti_bit then
-			_t = 32
-		 else
-			_t = 4
-		end
-		for i in pairs(S_list) do
-			local _N = S_list[i].internalName:gsub('^.*/', '')
-			if N_So[1] == _N and N_So[2] == S_list[i].state then
-				_S = S_list[i]
-				break
-			end
-		end
-		if _S then
-			t[#t + 1] = {}
-			t[#t].address = _S.start + Offset[1]
-			t[#t].flags = _t
-			if #Offset ~= 1 then
-				for i = 2, #Offset do
-					local S = gg.getValues(t)
-					t = {}
-					for _ in pairs(S) do
-						if not ti.x64 then
-							S[_].value = S[_].value & 0xFFFFFFFF
-						end
-						t[#t + 1] = {}
-						t[#t].address = S[_].value + Offset[i]
-						t[#t].flags = _t
-					end
-				end
-			end
-			_S = t[#t].address
-			
-		end
-		return _S
-	end
-	local _A = string.format('0x%X', Get_Address(t_So, t_Offset, _bit))
-	return _A
+local Ranges=gg.getRangesList('/')
+local function Read(module,type)
+    for k,v in pairs(Ranges) do
+        if v['internalName']:match('[^/]*$')==module and v['type']==type then
+            return v['start']
+        end
+    end
 end
 
+local Table={}
+local function Modify(address,value,flags)
+    Table[#Table+1]={address=address,value=value,flags=flags}
+end
 
-local t = {"libanogs.so:bss", "Cb"}
-local tt = {0x638}
-local ttt = S_Pointer(t, tt, true)
-gg.addListItems({{address = ttt, flags = 4, value = 64, freeze = true}})
+Modify(Read('libanogs.so:bss','rw-p')+0x3a8,4096,4)
+Modify(Read('libanogs.so:bss','rw-p')+0x638,4096,4)
+Modify(Read('libanogs.so:bss','rw-p')+0x6c8,4096,4)
+Modify(Read('libanogs.so:bss','rw-p')+0x1948,4096,4)
+Modify(Read('libanogs.so:bss','rw-p')+0x6210,2,4)
+gg.setValues(Table)
 
-
-function setvalue(address,flags,value)  local refinevalues={}  refinevalues[1]={}  refinevalues[1].address=address  refinevalues[1].flags=flags  refinevalues[1].value=value  gg.setValues(refinevalues)  end 
-so=gg.getRangesList('libanogs.so')[1].start 
-py=0x5093A8 setvalue(so+py,4,4096)
-py=0x509638 setvalue(so+py,4,4096)
-py=0x5096C8 setvalue(so+py,4,4096)
-py=0x50A948 setvalue(so+py,4,4096)
-py=0x50F210 setvalue(so+py,4,4096)
-setvalue(so + "0x14EED8 ", 4, "h C0 03 5F D6")--Crash Fix
-setvalue(so + "0x349F1C ", 4, "h C0 03 5F D6")--Crash Fix
-
-setvalue(so + "0xCB46C", 4, "h 00 00 00 00")
-setvalue(so + "0xCB470", 4, "h 00 00 00 00")
-setvalue(so + "0xCB474", 4, "h 00 00 00 00")
-setvalue(so + "0xCB478", 4, "h 00 00 00 00")
-
-setvalue(so + "0xCB504", 4, "h 00 00 00 00")
-setvalue(so + "0xCB508", 4, "h 00 00 00 00")
-setvalue(so + "0xCB4A4", 4, "h 00 00 00 00")
-setvalue(so + "0xCB390", 4, "h 00 00 00 00")
-setvalue(so + "0xCB394", 4, "h 00 00 00 00")
-setvalue(so + "0xCB3DC", 4, "h 00 00 00 00")
-
-gg.processResume()
 end
 
 
 
-
-function BP2()
-
-  function S_Pointer(t_So, t_Offset, _bit)
-	local function getRanges()
-		local ranges = {}
-		local t = gg.getRangesList('^/data/*.so*$')
-		for i, v in pairs(t) do
-			if v.type:sub(2, 2) == 'w' then
-				table.insert(ranges, v)
-			end
-		end
-		return ranges
-	end
-	local function Get_Address(N_So, Offset, ti_bit)
-		local ti = gg.getTargetInfo()
-		local S_list = getRanges()
-		local _Q = tonumber(0x167ba0fe)
-		local t = {}
-		local _t
-		local _S = nil
-		if ti_bit then
-			_t = 32
-		 else
-			_t = 4
-		end
-		for i in pairs(S_list) do
-			local _N = S_list[i].internalName:gsub('^.*/', '')
-			if N_So[1] == _N and N_So[2] == S_list[i].state then
-				_S = S_list[i]
-				break
-			end
-		end
-		if _S then
-			t[#t + 1] = {}
-			t[#t].address = _S.start + Offset[1]
-			t[#t].flags = _t
-			if #Offset ~= 1 then
-				for i = 2, #Offset do
-					local S = gg.getValues(t)
-					t = {}
-					for _ in pairs(S) do
-						if not ti.x64 then
-							S[_].value = S[_].value & 0xFFFFFFFF
-						end
-						t[#t + 1] = {}
-						t[#t].address = S[_].value + Offset[i]
-						t[#t].flags = _t
-					end
-				end
-			end
-			_S = t[#t].address
-			print(string.char(231,190,164,58).._Q)
-		end
-		return _S
-	end
-	local _A = string.format('0x%X', Get_Address(t_So, t_Offset, _bit))
-	return _A
-end
-
-
-
-local t = {"libUE4.so:bss", "Cb"}
-local tt = {0x29f3a0}
-local ttt = S_Pointer(t, tt, true)
-gg.addListItems({{address = ttt, flags = 4, value = 67109633, freeze = true}})
-local tt = {0x29f3f8}
-local ttt = S_Pointer(t, tt, true)
-gg.addListItems({{address = ttt, flags = 4, value = 67109633, freeze = true}})
-
-local t = {"libanogs.so:bss", "Cb"}
-local tt = {0x3a8}
-local ttt = S_Pointer(t, tt, true)
-gg.addListItems({{address = ttt, flags = 4, value = 67109633, freeze = true}})
-local tt = {0x2558}
-local ttt = S_Pointer(t, tt, true)
-gg.addListItems({{address = ttt, flags = 4, value = 67109633, freeze = true}})
-local tt = {0x638}
-local ttt = S_Pointer(t, tt, true)
-gg.addListItems({{address = ttt, flags = 4, value = 67109633, freeze = true}})
-
-local tt = {0x1948}
-local ttt = S_Pointer(t, tt, true)
-gg.addListItems({{address = ttt, flags = 4, value = 67109633, freeze = true}})
-local tt = {0x6210}
-local ttt = S_Pointer(t, tt, true)
-gg.addListItems({{address = ttt, flags = 4, value = 67109633, freeze = true}})
-
-
-end
-
-function EXIT()
-print("  ┏━━┓♡☆")
-print("┏┛□□┗┓♡")
-print("┗⊙━━⊙┛==")
-gg.skipRestoreState()
-gg.setVisible(true)
-os.exit()
-end
-
-cs = "21"
 while true do
 if gg.isVisible(true) then
 PUBGMH = 1
@@ -251,4 +45,3 @@ if PUBGMH == 1 then
 HOME()
 end
 end
-
