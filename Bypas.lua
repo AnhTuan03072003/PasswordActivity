8d9be76a19cfd59d3d1226fe09a255861e647238
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-08-22 15:35:10 +0700

    Bypas.lua

--- Bypas.lua
+++ Bypas.lua
@@ -4,7 +4,7 @@ arg = {...}
 
 local pas = gg.prompt
 
-({"🔑 KEY ĐƯỢC CẬP NHẬT LÚC: 21/08/2023 | 12H TRƯA MỖI NGÀY RESET KEY\n⚠️ VUI LÒNG XEM VIDEO HƯỚNG DẪN TRƯỚC KHI DÙNG\n👉 39K/1 THÁNG FREE KEY IB ADMIN TELEGRAM"}, {},{"text"})
+({"🔑 KEY ĐƯỢC CẬP NHẬT LÚC: 22/08/2023 | 12H TRƯA MỖI NGÀY RESET KEY\n⚠️ VUI LÒNG XEM VIDEO HƯỚNG DẪN TRƯỚC KHI DÙNG\n👉 39K/1 THÁNG FREE KEY IB ADMIN TELEGRAM"}, {},{"text"})
 
 pas = pas and pas[1] .. " " or os.exit(gg.alert("Thoát... Ấn Oke",""))
 
@@ -26,7 +26,7 @@ end
 
 end
 
-pass("hmghackk_cy3GvUImQw","t")
+pass("hmghackk_zwMZtM9F4m","t")
 
 
 function HOME()
