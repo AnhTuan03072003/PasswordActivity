47d3569101f1cc791e55ef11b9a6f843dc6cc881
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-08-23 08:17:59 +0700

    Bypas.lua

--- Bypas.lua
+++ Bypas.lua
@@ -19,13 +19,7 @@ setvalue(so + "0x349F1C ", 4, "h C0 03 5F D6")--Crash Fix
 gg.toast('Fix Crash Sansero + Protect Done ')
   
 
- 	 function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
-so=gg.getRangesList('libanogs.so')[1].start
-setvalue(so + "0x107360",32, "h 00 00 B0 E3 1E FF 2F E1")
-
-setvalue(so + "0x14EF38",4, "h C0 03 5F D6")
-setvalue(so + "0x14D4BC",4, "h C0 03 5F D6")
-
+ 	 
 
 
 
