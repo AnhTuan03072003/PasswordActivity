afeb12ec73beea4100d4a488c2307fcc2816c91e
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-08-23 08:29:46 +0700

    Bypas.lua

--- Bypas.lua
+++ Bypas.lua
@@ -12,15 +12,26 @@ end
 
 function BP1()
 
-function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end 
-so=gg.getRangesList('libanogs.so')[1].start
-setvalue(so + "0x14EED8 ", 4, "h C0 03 5F D6")--Crash Fix
-setvalue(so + "0x349F1C ", 4, "h C0 03 5F D6")--Crash Fix
-gg.toast('Fix Crash Sansero + Protect Done ')
-  
+local Ranges=gg.getRangesList('/')
+local function Read(module,type)
+    for k,v in pairs(Ranges) do
+        if v['internalName']:match('[^/]*$')==module and v['type']==type then
+            return v['start']
+        end
+    end
+end
 
- 	 
+local Table={}
+local function Modify(address,value,flags)
+    Table[#Table+1]={address=address,value=value,flags=flags}
+end
 
+Modify(Read('libanogs.so:bss','rw-p')+0x3a8,4096,4)
+Modify(Read('libanogs.so:bss','rw-p')+0x638,4096,4)
+Modify(Read('libanogs.so:bss','rw-p')+0x6c8,4096,4)
+Modify(Read('libanogs.so:bss','rw-p')+0x1948,4096,4)
+Modify(Read('libanogs.so:bss','rw-p')+0x6210,2,4)
+gg.setValues(Table)
 
 
 end
@@ -39,3 +50,4 @@ if PUBGMH == 1 then
 HOME()
 end
 end
+ 
