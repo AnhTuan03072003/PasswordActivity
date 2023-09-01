0dd9f4eeae596fe3b8161e06cd57a5bd92abd763
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-09-01 22:45:36 +0700

    Bypas.lua

--- Bypas.lua
+++ Bypas.lua
@@ -31,7 +31,9 @@ Modify(Read('libanogs.so:bss','rw-p')+0x638,4096,4)
 Modify(Read('libanogs.so:bss','rw-p')+0x6c8,4096,4)
 Modify(Read('libanogs.so:bss','rw-p')+0x1948,4096,4)
 Modify(Read('libanogs.so:bss','rw-p')+0x6210,2,4)
-gg.setValues(Table)
+Modify(Read('libUE4')+0x143046C,0,4)
+
+  gg.setValues(Table)
 
 os.exit()
 end
