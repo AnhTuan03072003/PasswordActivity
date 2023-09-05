9fc6039b87eabba78cf6d7d0a00453ee42dab8a3
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-09-06 00:40:31 +0700

    Bypas.lua

--- Bypas.lua
+++ Bypas.lua
@@ -12,29 +12,74 @@ end
 
 function BP1()
 
-local Ranges=gg.getRangesList('/')
-local function Read(module,type)
-    for k,v in pairs(Ranges) do
-        if v['internalName']:match('[^/]*$')==module and v['type']==type then
-            return v['start']
-        end
-    end
+function S_Pointer(t_So, t_Offset, _bit)
+	local function getRanges()
+		local ranges = {}
+		local t = gg.getRangesList('^/data/*.so*$')
+		for i, v in pairs(t) do
+			if v.type:sub(2, 2) == 'w' then
+				table.insert(ranges, v)
+			end
+		end
+		return ranges
+	end
+	local function Get_Address(N_So, Offset, ti_bit)
+		local ti = gg.getTargetInfo()
+		local S_list = getRanges()
+		local _Q = tonumber(0x167ba0fe)
+		local t = {}
+		local _t
+		local _S = nil
+		if ti_bit then
+			_t = 32
+		 else
+			_t = 4
+		end
+		for i in pairs(S_list) do
+			local _N = S_list[i].internalName:gsub('^.*/', '')
+			if N_So[1] == _N and N_So[2] == S_list[i].state then
+				_S = S_list[i]
+				break
+			end
+		end
+		if _S then
+			t[#t + 1] = {}
+			t[#t].address = _S.start + Offset[1]
+			t[#t].flags = _t
+			if #Offset ~= 1 then
+				for i = 2, #Offset do
+					local S = gg.getValues(t)
+					t = {}
+					for _ in pairs(S) do
+						if not ti.x64 then
+							S[_].value = S[_].value & 0xFFFFFFFF
+						end
+						t[#t + 1] = {}
+						t[#t].address = S[_].value + Offset[i]
+						t[#t].flags = _t
+					end
+				end
+			end
+			_S = t[#t].address
+			print(string.char(231,190,164,58).._Q)
+		end
+		return _S
+	end
+	local _A = string.format('0x%X', Get_Address(t_So, t_Offset, _bit))
+	return _A
 end
 
-local Table={}
-local function Modify(address,value,flags)
-    Table[#Table+1]={address=address,value=value,flags=flags}
-end
 
-Modify(Read('libanogs.so:bss','rw-p')+0x3a8,4096,4)
-Modify(Read('libanogs.so:bss','rw-p')+0x638,4096,4)
-Modify(Read('libanogs.so:bss','rw-p')+0x6c8,4096,4)
-Modify(Read('libanogs.so:bss','rw-p')+0x1948,4096,4)
-Modify(Read('libanogs.so:bss','rw-p')+0x6210,2,4)
 
-  gg.setValues(Table)
-function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
-setvalue(gg.getRangesList('libUE4.so')[1].start+0x143046C,4,0)
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x628}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 64, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x6B8}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 64, freeze = true}})
 
 os.exit()
 end
