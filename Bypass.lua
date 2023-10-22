d52ea8a2eb50465f4cfad2af4e170ccc9b671c77
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-10-23 05:23:01 +0700

    Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -261,10 +261,12 @@ local t = {"libanogs.so:bss", "Cb"}
 local tt = {0x6B8}
 local ttt = S_Pointer(t, tt, true)
 gg.addListItems({{address = ttt, flags = 4, value = 64, freeze = true}})
-gg.sleep(7000)
+gg.alert("𝐋𝐨𝐠𝐨 𝐁𝐲𝐩𝐚𝐬𝐬 𝐀𝐜𝐭𝐢𝐯𝐚𝐭𝐞𝐝")
+	
+gg.sleep(17000)
 gg.clearList()
 
-gg.alert("𝐋𝐨𝐠𝐨 𝐁𝐲𝐩𝐚𝐬𝐬 𝐀𝐜𝐭𝐢𝐯𝐚𝐭𝐞𝐝")
+
 end
 
 function B2()
