cb39ba3769d95726095bb5d4bbccba373ad3ed1e
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-11-11 08:44:17 +0700

    Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -78,8 +78,6 @@ local tt = {0x628}
 local ttt = S_Pointer(t, tt, true)
 gg.addListItems({{address = ttt, flags = 4, value = 64, freeze = true}})
 
-function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
-function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
 
 
 local t = {"libanogs.so", "Cd"}
@@ -238,6 +236,8 @@ local ttt = S_Pointer(t, tt, true)
 gg.addListItems({{address = ttt, flags = 4, value = 17024, freeze = true}})
 
 
+function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
+function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
 	
 so=gg.getRangesList('libanogs.so')[1].start
 setvalue(so + "0x155210", 32, "h 00 00 B0 E3 1E FF 2F E1")
