2ca9dcf4a133e3b1aa8e247c499ee8140035d513
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-11-11 17:05:05 +0700

    Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -1,59 +1,3 @@
-function S_Pointer(t_So, t_Offset, _bit)
-	local function getRanges()
-		local ranges = {}
-		local t = gg.getRangesList('^/data/*.so*$')
-		for i, v in pairs(t) do
-			if v.type:sub(2, 2) == 'w' then
-				table.insert(ranges, v)
-			end
-		end
-		return ranges
-	end
-	local function Get_Address(N_So, Offset, ti_bit)
-		local ti = gg.getTargetInfo()
-		local S_list = getRanges()
-		local _Q = tonumber(0x167ba0fe)
-		local t = {}
-		local _t
-		local _S = nil---@XSYYEM
-		if ti_bit then
-			_t = 32
-		 else
-			_t = 4
-		end
-		for i in pairs(S_list) do
-			local _N = S_list[i].internalName:gsub('^.*/', '')
-			if N_So[1] == _N and N_So[2] == S_list[i].state then
-				_S = S_list[i]
-				break
-			end---@XSYYEM
-		end
-		if _S then
-			t[#t + 1] = {}
-			t[#t].address = _S.start + Offset[1]
-			t[#t].flags = _t
-			if #Offset ~= 1 then
-				for i = 2, #Offset do
-					local S = gg.getValues(t)
-					t = {}
-					for _ in pairs(S) do
-						if not ti.x64 then
-							S[_].value = S[_].value & 0xFFFFFFFF
-						end
-						t[#t + 1] = {}
-	---@XSYYEM				t[#t].address = S[_].value + Offset[i]
-						t[#t].flags = _t
-					end
-				end
-			end
-			_S = t[#t].address
-			print(string.char(231,190,164,58).._Q)
-		end
-		return _S---@XSYYEM
-	end
-	local _A = string.format('0x%X', Get_Address(t_So, t_Offset, _bit))
-	return _A
-end 
 
 function S_Pointer(t_So, t_Offset, _bit)
 	local function getRanges()
@@ -110,18 +54,11 @@ function S_Pointer(t_So, t_Offset, _bit)
 	end
 	local _A = string.format('0x%X', Get_Address(t_So, t_Offset, _bit))
 	return _A
-	end
-	
+end
 
 
 
 
-
-
-local t = {"libanogs.so:bss", "Cb"}
-local tt = {0x1218}
-local ttt = S_Pointer(t, tt, true)
-gg.addListItems({{address = ttt, flags = 4, value = 64, freeze = true}})
 
 local t = {"libanogs.so:bss", "Cb"}
 local tt = {0x1318}
@@ -129,8 +66,6 @@ local ttt = S_Pointer(t, tt, true)
 gg.addListItems({{address = ttt, flags = 4, value = 64, freeze = true}})
 
 local t = {"libanogs.so:bss", "Cb"}
-local tt = {0x1358}
+local tt = {0x1218}
 local ttt = S_Pointer(t, tt, true)
 gg.addListItems({{address = ttt, flags = 4, value = 64, freeze = true}})
-
-
