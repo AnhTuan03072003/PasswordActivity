8a73d1542494d7cddbf8ebb037a3f6cd33784699
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-11-14 11:59:52 +0700

    Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -6,4 +6,20 @@ local so = gg.getRangesList('libanogs.so:bss')[1].start
 local py = 0x13B0
 setvalue(so + py, 64, 4)
 
+local so = gg.getRangesList('libanogs.so:bss')[1].start
+local py = 0x13B0
+setvalue(so + py, 64, 4)
+
+local so = gg.getRangesList('libanogs.so:bss')[1].start
+local py = 0x13B0
+setvalue(so + py, 64, 4)
+
+local so = gg.getRangesList('libanogs.so:bss')[1].start
+local py = 0x13B0
+setvalue(so + py, 64, 4)
+
+local so = gg.getRangesList('libanogs.so:bss')[1].start
+local py = 0x13B0
+setvalue(so + py, 64, 4)
+
 gg.alert("Fix Crash Thành Công 👍")
