6e3ff7bdbedd20fb6167e106310bf6eb0e8b2773
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-11-15 17:49:02 +0700

    Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -1,3 +1,22 @@
+HOME = 1
+function HOME()
+VIPONLY = gg.multiChoice({
+"➥Logo 1st + Fix Văng Game 🛡",
+"➥Lobby [Sảnh Chờ] 🛡",
+
+}, nil, "𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 :- @hmghak\nKHÔNG BẮN BOT - BẮN DƯỚI 10 KILL\nRESET GAME SAU 2 TRẬN\nBẮN DƯỚI 150M")
+
+if VIPONLY == nil then else
+if VIPONLY[1]== true then B1() end
+if VIPONLY[2]== true then B2() end
+end
+PUBGMH = -1
+end
+
+function B1()
+
+
+
 function setvalue(address, value, flags)
  gg.setValues({{address = address, flags = flags, value = value}})
 end
@@ -6,20 +25,64 @@ local so = gg.getRangesList('libanogs.so:bss')[1].start
 local py = 0x13B0
 setvalue(so + py, 64, 4)
 
-local so = gg.getRangesList('libanogs.so:bss')[1].start
-local py = 0x13B0
-setvalue(so + py, 64, 4)
+gg.alert("Fix Văng Game √")
+end
 
-local so = gg.getRangesList('libanogs.so:bss')[1].start
-local py = 0x13B0
-setvalue(so + py, 64, 4)
+function B2()
+gg.alert("👉 Chưa Ổn Đỉnh Khi Chơi Aimbot\n👉 Khuyến Khích Không Bật Khi Chỉ Chơi Esp")
 
-local so = gg.getRangesList('libanogs.so:bss')[1].start
-local py = 0x13B0
-setvalue(so + py, 64, 4)
 
-local so = gg.getRangesList('libanogs.so:bss')[1].start
-local py = 0x13B0
-setvalue(so + py, 64, 4)
+function setvalue(address, value, flags)
+ gg.setValues({{address = address, flags = flags, value = value}})
+end
 
-gg.alert("Fix Crash Thành Công 👍")
+function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
+
+so=gg.getRangesList('libanogs.so')[1].start
+
+
+
+setvalue(so + "0x279818", 4, "00 20 70 47");
+ setvalue(so + "0x27964C", 4, "00 20 70 47");
+ setvalue(so + "0x279A94", 4, "00 20 70 47");
+ setvalue(so + "0x279790", 4, "00 20 70 47");
+ setvalue(so + "0x279720", 4, "00 20 70 47");
+ setvalue(so + "0x2793D0", 4, "00 20 70 47");
+ setvalue(so + "0x279420", 4, "00 20 70 47");
+ setvalue(so + "0x27946C", 4, "00 20 70 47");
+ setvalue(so + "0x279490", 4, "00 20 70 47");
+ setvalue(so + "0x279518", 4, "00 20 70 47");
+ setvalue(so + "0x279580", 4, "00 20 70 47"); 
+ setvalue(so + "0x2795D0", 4, "00 20 70 47");
+
+function setvalue(address, value, flags)
+ gg.setValues({{address = address, flags = flags, value = value}})
+end
+so=gg.getRangesList('libUE4.so')[1].start
+local py = 0x940
+setvalue(so+ py, 0, 4)
+local py = 0x987
+setvalue(so+ py, 0, 4)
+
+
+gg.alert("Bypass Lobby Done √")
+os.exit()
+end
+
+
+
+
+while true do
+if gg.isVisible(true) then
+PUBGMH = 1
+gg.setVisible(false)
+end
+if PUBGMH == 1 then
+HOME()
+end
+end
+
+
+
+
+
