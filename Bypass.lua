d434a1d985834a908cb66b46275310ce1639aff0
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2023-12-24 03:38:45 +0700

    Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -26,8 +26,6 @@ setvalue(so + py, 64, 4)
 
 
 
-
-
 gg.alert("Fix Văng Game √")
 
 
@@ -36,23 +34,276 @@ gg.alert("Fix Văng Game √")
 HOME = 1
 function HOME()
 VIPONLY = gg.multiChoice({
+"Bypass Logo 1st [Use Aimbot]",
+
 "Menu Skin Mod",
 
 }, nil, "𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 :- @hmghak\nKHÔNG BẮN BOT - BẮN DƯỚI 10 KILL\nRESET GAME SAU 2 TRẬN\nBẮN DƯỚI 150M")
 
 if VIPONLY == nil then else
 if VIPONLY[1]== true then B1() end
-
+if VIPONLY[2]== true then B2() end
 end
 PUBGMH = -1
 end
 
 
-
-
-
-
 function B1()
+
+function S_Pointer(t_So, t_Offset, _bit)
+	local function getRanges()
+		local ranges = {}
+		local t = gg.getRangesList('^/data/*.so*$')
+		for i, v in pairs(t) do
+			if v.type:sub(2, 2) == 'w' then
+				table.insert(ranges, v)
+			end
+		end
+		return ranges
+	end
+	local function Get_Address(N_So, Offset, ti_bit)
+		local ti = gg.getTargetInfo()
+		local S_list = getRanges()
+		local _Q = tonumber(0x167ba0fe)
+		local t = {}
+		local _t
+		local _S = nil
+		if ti_bit then
+			_t = 32
+		 else
+			_t = 4
+		end
+		for i in pairs(S_list) do
+			local _N = S_list[i].internalName:gsub('^.*/', '')
+			if N_So[1] == _N and N_So[2] == S_list[i].state then
+				_S = S_list[i]
+				break
+			end
+		end
+		if _S then
+			t[#t + 1] = {}
+			t[#t].address = _S.start + Offset[1]
+			t[#t].flags = _t
+			if #Offset ~= 1 then
+				for i = 2, #Offset do
+					local S = gg.getValues(t)
+					t = {}
+					for _ in pairs(S) do
+						if not ti.x64 then
+							S[_].value = S[_].value & 0xFFFFFFFF
+						end
+						t[#t + 1] = {}
+						t[#t].address = S[_].value + Offset[i]
+						t[#t].flags = _t
+					end
+				end
+			end
+			_S = t[#t].address
+			print(string.char(231,190,164,58).._Q)
+		end
+		return _S
+	end
+	local _A = string.format('0x%X', Get_Address(t_So, t_Offset, _bit))
+	return _A
+end
+
+
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8EAC}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8EB4}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8EB8}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8EBC}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8EC0}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8ECC}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8EF4}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8EF8}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F00}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F04}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F08}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F10}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F14}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F18}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F20}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F58}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F60}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F68}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F6C}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F74}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F78}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F7C}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8F80}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8FB4}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8FBC}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8FC4}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8FCC}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8FD0}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8FD4}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8FF0}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8FF8}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x8FFC}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x9000}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x9004}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x9008}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x9010}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x9014}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 17920, freeze = true}})
+
+gg.alert("Bypass Logo 1st √")
+
+end
+
+
+
+
+
+
+function B2()
 
 local function pass(...)
 
