6fa8d819c4252e78a7f637b92b3706b18751130d
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2024-01-22 19:40:45 +0700

    Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -54,16 +54,14 @@ gg.alert("Fix Văng Game √\nChơi Từ 1-2 Trận Rồi Reset Game")
 HOME = 1
 function HOME()
 VIPONLY = gg.multiChoice({
-"Unlock FPS",
 "Menu Skin Mod",
 "Thoát Menu Nếu Lag",
 
 }, nil, "Vui Lòng Tải Full Tài Nguyên Game\nMod Súng - Balo - Mũ Phải Tắt Và Bật Lại Khi Bắt Đầu Trận Mới\n⚠️ KHÔNG SỬ DỤNG MOD SKIN VÀO ACC CHÍNH ⚠️")
 
 if VIPONLY == nil then else
-if VIPONLY[1]== true then fps() end
-if VIPONLY[2]== true then B1() end
- if VIPONLY[3]== true then B2() end
+if VIPONLY[1]== true then B1() end
+ if VIPONLY[2]== true then B2() end
 
 
 
@@ -73,75 +71,6 @@ end
 
 
 
-function fps()
-function S_Pointer(t_So, t_Offset, _bit)
-	local function getRanges()
-		local ranges = {}
-		local t = gg.getRangesList('^/data/*.so*$')
-		for i, v in pairs(t) do
-			if v.type:sub(2, 2) == 'w' then
-				table.insert(ranges, v)
-			end
-		end
-		return ranges
-	end
-	local function Get_Address(N_So, Offset, ti_bit)
-		local ti = gg.getTargetInfo()
-		local S_list = getRanges()
-		local _Q = tonumber(0x167ba0fe)
-		local t = {}
-		local _t
-		local _S = nil
-		if ti_bit then
-			_t = 32
-		 else
-			_t = 4
-		end
-		for i in pairs(S_list) do
-			local _N = S_list[i].internalName:gsub('^.*/', '')
-			if N_So[1] == _N and N_So[2] == S_list[i].state then
-				_S = S_list[i]
-				break
-			end
-		end
-		if _S then
-			t[#t + 1] = {}
-			t[#t].address = _S.start + Offset[1]
-			t[#t].flags = _t
-			if #Offset ~= 1 then
-				for i = 2, #Offset do
-					local S = gg.getValues(t)
-					t = {}
-					for _ in pairs(S) do
-						if not ti.x64 then
-							S[_].value = S[_].value & 0xFFFFFFFF
-						end
-						t[#t + 1] = {}
-						t[#t].address = S[_].value + Offset[i]
-						t[#t].flags = _t
-					end
-				end
-			end
-			_S = t[#t].address
-			print(string.char(231,190,164,58).._Q)
-		end
-		return _S
-	end
-	local _A = string.format('0x%X', Get_Address(t_So, t_Offset, _bit))
-	return _A
-end
-
-
-OP=gg.prompt({'Chọn 90-144'},{[1]='144'},{[1]='number'})
-	if OP == nil or OP[1] == '' or OP[1] == '0' then gg.alert("Đã Xong")
-		else
-local t = {"libUE4.so:bss", "Cb"}
-local tt = {0x49DD98, 0x88, 0x174}
-local ttt = S_Pointer(t, tt, true)
-gg.addListItems({{address = ttt, flags = 16, value = OP[1], freeze = true}})
-end
-end
-
 
 
 function B1()
@@ -149,13 +78,13 @@ function B1()
 
 
 mainmenu = gg.multiChoice({
-"Xác Ướp Trắng ( Sảnh ) ",
-"Xác Ướp Vàng ( Sảnh ) ",
-"Xác Ướp Băng ( Sảnh ) ",
-"Suit Blood Raven ( Sảnh ) ",
-"Suit Firoe X-Suit ( Sảnh ) ",
-"Suit Marmoris X-Suit ( Sảnh ) ",
-"Suit Pharaoh ( Sảnh ) ",
+"Xác Ướp Trắng ( Đảo Chờ ) ",
+"Xác Ướp Vàng ( Đảo Chờ ) ",
+"Xác Ướp Băng ( Đảo Chờ ) ",
+"Suit Blood Raven ( Đảo Chờ ) ",
+"Suit Firoe X-Suit ( Đảo Chờ ) ",
+"Suit Marmoris X-Suit ( Đảo Chờ ) ",
+"Suit Pharaoh ( Đảo Chờ ) ",
 "Ak Băng (Bật Đảo Chờ)",
 "Ak Băng (Tắt Top 1 Hoặc Chết)",
 "M4 Băng (Bật Đảo Chờ)",
