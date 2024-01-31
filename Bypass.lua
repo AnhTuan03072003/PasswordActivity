520c4dbbb2b6f2741d00b165636819f8360563f3
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2024-01-31 21:15:16 +0700

    Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -4,8 +4,8 @@ function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=addres
 function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
 
 so=gg.getRangesList('libanogs.so:bss')[1].start
-py=0xA58
-setvalue(so+py,4, 64)
+py=0x9C8
+setvalue(so+py,4, 4096)
 
 
 
