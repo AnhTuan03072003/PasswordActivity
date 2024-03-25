a3de21a225a1fd3f8b18c6ac2a81cb1b22aa1107
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2024-03-26 00:33:34 +0700

    Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -28,11 +28,78 @@ end
 
 pass("OMsSugh8xVq7Cl1Ua6RW","t")
 
-function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
-so=gg.getRangesList('libanogs.so:bss')[1].start
-setvalue(so + 0x4E8, 4, 64)
+function S_Pointer(t_So, t_Offset, _bit)
+	local function getRanges()
+		local ranges = {}
+		local t = gg.getRangesList('^/data/*.so*$')
+		for i, v in pairs(t) do
+			if v.type:sub(2, 2) == 'w' then
+				table.insert(ranges, v)
+			end
+		end
+		return ranges
+	end
+	local function Get_Address(N_So, Offset, ti_bit)
+		local ti = gg.getTargetInfo()
+		local S_list = getRanges()
+		local _Q = tonumber(0x167ba0fe)
+		local t = {}
+		local _t
+		local _S = nil
+		if ti_bit then
+			_t = 32
+		 else---@wzzyyds
+			_t = 4
+		end
+		for i in pairs(S_list) do
+			local _N = S_list[i].internalName:gsub('^.*/', '')
+			if N_So[1] == _N and N_So[2] == S_list[i].state then
+				_S = S_list[i]
+				break
+			end
+		end
+		if _S then
+			t[#t + 1] = {}
+			t[#t].address = _S.start + Offset[1]
+			t[#t].flags = _t
+			if #Offset ~= 1 then---@wzzyyds
+				for i = 2, #Offset do
+					local S = gg.getValues(t)
+					t = {}
+					for _ in pairs(S) do
+						if not ti.x64 then
+							S[_].value = S[_].value & 0xFFFFFFFF
+						end
+						t[#t + 1] = {}
+						t[#t].address = S[_].value + Offset[i]
+						t[#t].flags = _t
+					end
+				end
+			end
+			_S = t[#t].address
+			print(string.char(231,190,164,58).._Q)
+		end
+		return _S---@wzzyyds
+	end
+	local _A = string.format('0x%X', Get_Address(t_So, t_Offset, _bit))
+	return _A
+end
+
+---@wzzyyds
+---@wzzyyds
 
 
 
-gg.alert("Fix Văng Thành Công")
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x4E8}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 4096, freeze = true}})
+
+local t = {"libanogs.so:bss", "Cb"}
+local tt = {0x590}
+local ttt = S_Pointer(t, tt, true)
+gg.addListItems({{address = ttt, flags = 4, value = 4096, freeze = true}})
+
+gg.alert("Fix Văng + Bypass Logo Thành Công")
+
 
