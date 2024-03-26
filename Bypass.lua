289e577c1eea5b4c3925bed45edda6054ab2381d
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2024-03-26 13:20:31 +0700

    Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -57,7 +57,7 @@ end
 
 function LOGO()
 function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
-gg.toast("Load 5 giây")
+gg.toast("Load 2 giây")
   gg.sleep(1999)
 so=gg.getRangesList('libanogs.so:bss')[1].start
 setvalue(so + 0x4E8, 4, 64)
