af5858211671e5a6f7120752a6d2828434d702f3
AnhTuan03072003 <115359353+AnhTuan03072003@users.noreply.github.com>
2024-04-15 13:27:59 +0700

    Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -33,26 +33,313 @@ pass("nXvjMrgdTtf8CEi23GZ5","t")
 
 
 
+
+
+
+
+
+
 HOME = 1
 function HOME()
 VIPONLY = gg.multiChoice({
 "BYPASS LOGO THỨ 1",
-"🛡️CHECK ANTIBAN [SẢNH CHỜ]",
-"BYPASS ISLAND ON", 
-"BYPASS ISLAND OFF",
+"BYPASS LOBBY [SẢNH]",
 "Admin",
-
+"Thoát Nếu Lag",
 
 
 }, nil, "Logo 1st thường load ~ 20 giây\n⚠️ Reset Game Sau 1-2 Trận Đấu")
 
 if VIPONLY == nil then else
   if VIPONLY[1]== true then LOGO() end
-    if VIPONLY[2]== true then CHECK() end
-if VIPONLY[3]== true then ON() end
- if VIPONLY[4]== true then OFF() end
- if VIPONLY[5]== true then admin() end
+  if VIPONLY[2]== true then LOBBY() end
+     
+ if VIPONLY[3]== true then admin() end
 
+     
+ if VIPONLY[4]== true then exitt() end
+
+
+
+
+end
+PUBGMH = -1
+end
+
+function admin()
+local function pass(...)
+
+arg = {...}
+
+local pas = gg.prompt
+
+({"Vui Lòng Nhập Key"}, {},{"text"})
+
+pas = pas and pas[1] .. " " or os.exit(gg.alert("Thoát... Ấn Oke",""))
+
+for i = 1, #arg do
+
+if arg[i] .. " " ==  pas then
+
+return gg.toast("Nhập Key Thành Công ✅")
+
+end
+
+end
+
+while (true) do
+
+os.exit(gg.alert("Key Sai Hoặc Hết Hạn❎", ""))
+
+end 
+
+end
+
+pass("","t")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("403003", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(50000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1405623", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("xac uop vang")
+
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("10100400", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.searchNumber("10100400", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(9999, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1101004046", gg.TYPE_DWORD)
+gg.clearResults()
+gg.searchNumber("10100400", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(9999, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1101004046", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("20300800", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(9999, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1,010,040,462", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("20500200", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(9999, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1,010,040463", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("20500500", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.processResume()
+gg.getResults(9999, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1,010,040463", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("29100400", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(9999, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1010040461", gg.TYPE_DWORD)
+gg.clearResults()
+
+gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1501001174", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+
+gg.searchNumber("501002", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1501002174", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+
+gg.searchNumber("501003", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1501003174", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("501004", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+
+gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1501001174", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("501005", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+
+gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1501002174", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("501006", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+
+gg.getResults(1000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1501003174", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("10100100", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.searchNumber("10100100", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(9999, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1101001089", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("29100100", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(9999, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1010010891", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("502001", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1502003014", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("502004", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1502003014", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("502002", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1502003014", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("502005", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1502003014", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("502003", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(500, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll("1502003014", gg.TYPE_DWORD)
+gg.clearResults()
+end
+
+
+function LOBBY()
+
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("131331;133,634", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("131,331", gg.TYPE_DWORD)
+    gg.getResults(5000)
+    gg.editAll("67,175,171", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134,914;144,387", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134,914", gg.TYPE_DWORD)
+gg.getResults(5000)
+gg.editAll("67,175,171", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134,658;134,658", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("134,658", gg.TYPE_DWORD)
+gg.getResults(5000)
+gg.editAll("67,175,171", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("144,387;133,634", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("144,387", gg.TYPE_DWORD)
+gg.getResults(500000)
+gg.editAll("67,175,171", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("133,378;144,387", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("133,378", gg.TYPE_DWORD)
+gg.getResults(500000)
+gg.editAll("67,175,171", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("196,864;16,842", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("196,864", gg.TYPE_DWORD)
+gg.getResults(500000)
+gg.editAll("67,175,171", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("262,144;262,149", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("262,149", gg.TYPE_DWORD)
+gg.getResults(500000)
+gg.editAll("67,175,171", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("262,400;262,656",gg.TYPE_QWORD)
+gg.refineNumber("262,400",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("67,175,171",gg.TYPE_QWORD)
+gg.clearResults()
+gg.alert("Bypass Lobby Thành Công")
+end
+
+
+function LOGO()
+function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
+function SearchWrite(Search, Write, Type)gg.clearResults()gg.setVisible(false)gg.searchNumber(Search[1][1], Type)local count = gg.getResultCount()local result = gg.getResults(count)gg.clearResults()local data = {}local base = Search[1][2]if (count > 0) then for i, v in ipairs(result) do v.isUseful = true end for k=2, #Search do local tmp = {} local offset = Search[k][2] - base local num = Search[k][1] for i, v in ipairs(result) do tmp[#tmp+1] = {} tmp[#tmp].address = v.address + offset tmp[#tmp].flags = v.flags  end tmp = gg.getValues(tmp) for i, v in ipairs(tmp) do if ( tostring(v.value) ~= tostring(num) ) then result[i].isUseful = false end end end for i, v in ipairs(result) do if (v.isUseful) then data[#data+1] = v.address end end if (#data > 0) then gg.toast("found "..#data.." data") local t = {} local base = Search[1][2] for i=1, #data do for k, w in ipairs(Write) do offset = w[2] - base t[#t+1] = {} t[#t].address = data[i] + offset t[#t].flags = Type t[#t].value = w[1] if (w[3] == true) then local item = {} item[#item+1] = t[#t] item[#item].freeze = true gg.addListItems(item) end end end gg.setValues(t) else gg.toast("not found", false) return false end else gg.toast("Not Found")  return false end end
+local Ranges=gg.getRangesList('/')
+local function Read(module,type)
+    for k,v in pairs(Ranges) do
+        if v['internalName']:match('[^/]*$')==module and v['type']==type then
+            return v['start']
+        end
+    end
+end
+local Table={}
+local function Modify(address,value,flags)
+    Table[#Table+1]={address=address,value=value,flags=flags}
+end
+gg.setVisible(false)
+-------- Termination Fixer -------
+Modify(Read('libanogs.so','r-xp')+0x4f94, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fa8, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fac, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fb0, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fb4, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fbc, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fc0, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fc4, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fc8, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fcc, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fd0, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fd4, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fd8, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fdc, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fe0, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fe4, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fe8, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4fec, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4ff0, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4ff4, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4ff8, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+Modify(Read('libanogs.so','r-xp')+0x4ffc, "h 7F 45 4C 46 F9 EA 0D 00", 4) 
+-------- 1Day Fixx Offsets
+Modify(Read('libanogs.so','r-xp')+0x4CF96FC, "h 00 00 A0 E3 1E FF 2F E1", 4)
+Modify(Read('libanogs.so','r-xp')+0x4CF960C, "h 00 00 A0 E3 1E FF 2F E1", 4)
+Modify(Read('libanogs.so','r-xp')+0x4CF9500, "h 00 00 A0 E3 1E FF 2F E1", 4)
+Modify(Read('libanogs.so','r-xp')+0x4CF9420, "h 00 00 A0 E3 1E FF 2F E1", 4)
+Modify(Read('libanogs.so','r-xp')+0x4CF9940, "h 00 00 A0 E3 1E FF 2F E1", 4)
+Modify(Read('libanogs.so','r-xp')+0x4CF9A50, "h 00 00 A0 E3 1E FF 2F E1", 4)
+
+function setvalue(address,flags,value) local tt={} tt[1]={} tt[1].address=address tt[1].flags=flags tt[1].value=value gg.setValues(tt) end
+
+so=gg.getRangesList('libanogs.so:bss')[1].start
+setvalue(so + 0x4E8, 4, 64)
+so=gg.getRangesList('libanogs.so:bss')[1].start
+setvalue(so + 0x590, 4, 64)
+
+
+
+
+  
+gg.alert("Fix Văng + Bypass Logo Thành Công")
+   
+end
+
+
+
+function exitt()
+os.exit()
+end
+
+
+
+while true do
+if gg.isVisible(true) then
+PUBGMH = 1
+gg.setVisible(false)
+end
+if PUBGMH == 1 then
+HOME()
+end
+end
 
 
 
